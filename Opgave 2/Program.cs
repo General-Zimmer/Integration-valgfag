272f40ab037cb568174f57e781aa5ada4e43cbd9
GZimmer <kristianz1000@gmail.com>
2024-09-11 11:54:55 +0200

    Opdate from laptop

---
+++ Opgave 2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Messaging;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Opgave_2
+{
+    internal class Program
+    {
+        class Plane
+        {
+            public long PlaneNo { get; set; }
+            public string Gate { get; set; }
+            public DateTime ArrivalTime { get; set; }
+            public DateTime DepartureTime { get; set; }
+
+            public Plane(long planeNo, string gate, DateTime arrivalTime, DateTime departureTime)
+            {
+                PlaneNo = planeNo;
+                Gate = gate;
+                ArrivalTime = arrivalTime;
+                DepartureTime = departureTime;
+            }
+
+                public override string ToString()
+                {
+                    return "PlaneNo: " + PlaneNo + " Gate: " + Gate + " ArrivalTime: " + ArrivalTime + " DepartureTime: " + DepartureTime;
+                }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Opgave 1
+            // Plane {
+            //      PlaneNo: bigInt
+            //      Gate: string
+            //      ArrivalTime: Datetime
+            //      DepartureTime: Datetime
+
+
+            // Opgave 2
+
+            Plane p = new Plane(123456789, "A1", DateTime.Now.AddDays(10), DateTime.Now.AddDays(11));
+
+            string planeJson = JsonConvert.SerializeObject(p);
+
+            MessageQueue messageQueue = null;
+            if (!MessageQueue.Exists(@".\Private$\L7AirplaneArrivals"))
+                MessageQueue.Create(@".\Private$\L7AirplaneArrivals");
+            messageQueue = new MessageQueue(@".\Private$\L7AirplaneArrivals");
+            messageQueue.Send(planeJson, p.PlaneNo.ToString());
+
+            
+            String message = messageQueue.Receive().Body.ToString();
+
+            Plane pBack = JsonConvert.DeserializeObject<Plane>(message);
+            
+            Console.WriteLine(message);
+            Console.WriteLine(pBack.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Opgave 3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+using Newtonsoft.Json;
+
+namespace Opgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Plane p = new Plane(123456789, "A1", DateTime.Now.AddDays(10), DateTime.Now.AddDays(11));
+
+            string planeJson = JsonConvert.SerializeObject(p);
+
+            List<String> airlinesNames = new List<String>();
+            airlinesNames.Add("SAS");
+            airlinesNames.Add("SWA");
+            airlinesNames.Add("KLM");
+
+            // This queue is here because there's formatting issues if the queues are remade later on, so reusing it is. 
+            List<MessageQueue> airlineQueues = new List<MessageQueue>();
+
+            MessageQueue messageQueue;
+            // Setup airline
+            foreach (var airline in airlinesNames)
+            {
+                String airlinePath = @".\Private$\L7" + airline;
+                if (!MessageQueue.Exists(airlinePath))
+                    MessageQueue.Create(airlinePath);
+
+                messageQueue = new MessageQueue(airlinePath);
+                airlineQueues.Add(messageQueue);
+                messageQueue.Label = "AirplaneArrivals_" + airline;
+            }
+
+            // Send info to airlines
+            foreach (var airlineQueue in airlineQueues)
+            {
+                airlineQueue.Send(planeJson, "AirplaneArrivals");
+            }
+
+            // Receive info from airlines
+            foreach (var airlineQueue in airlineQueues)
+            {
+                String message = airlineQueue.Receive().Body.ToString();
+                Plane pBack = JsonConvert.DeserializeObject<Plane>(message);
+
+                Console.WriteLine(message);
+                Console.WriteLine(pBack.ToString());
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+    class Plane
+    {
+        public long PlaneNo { get; set; }
+        public string Gate { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+        public Plane(long planeNo, string gate, DateTime arrivalTime, DateTime departureTime)
+        {
+            PlaneNo = planeNo;
+            Gate = gate;
+            ArrivalTime = arrivalTime;
+            DepartureTime = departureTime;
+        }
+
+        public override string ToString()
+        {
+            return "PlaneNo: " + PlaneNo + " Gate: " + Gate + " ArrivalTime: " + ArrivalTime + " DepartureTime: " + DepartureTime;
+        }
+    }
+}
